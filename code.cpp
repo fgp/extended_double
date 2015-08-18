6c2384314f9d24ca5565a86ff412aded22619c22
Florian G. Pflug <fgp@phlo.org>
2015-08-19 00:44:21 +0200

    Added code.cpp, which is used to inspected the generated assembly

--- code.cpp
+++ code.cpp
@@ -1,9 +1,17 @@
-//
-//  code.cpp
-//  Project
-//
-//  Created by Florian Pflug on 18.08.2015.
-//
-//
+/*
+ *  tests.cpp
+ *  extended_double
+ *
+ *  Created by Florian Pflug on 17.08.2015.
+ *  Copyright (c) 2015 Florian Pflug. All rights reserved.
+ */
 
-#include "code.h"
+#include "extended_double.h"
+
+extended_double multiply(extended_double a, extended_double b) {
+    return a * b * a * b;
+}
+
+extended_double add(extended_double a, extended_double b) {
+    return a + b + b + a;
+}
