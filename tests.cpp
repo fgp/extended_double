572bde478ca40b4436766b2c49441cbb165ad639
Florian Pflug <florian.pflug@univie.ac.at>
2015-08-18 16:56:25 +0200

    Keep m_fraction fully normalized, and added tests for +/- Inf behaviour.

--- tests.cpp
+++ tests.cpp
@@ -12,6 +12,38 @@
 
 BOOST_AUTO_TEST_SUITE(extended_double_tests)
 
+BOOST_AUTO_TEST_CASE(non_finite) {
+	const extended_double v_pinf(std::numeric_limits<double>::infinity());
+	const extended_double v_ninf(-std::numeric_limits<double>::infinity());
+
+	BOOST_CHECK_EQUAL(v_pinf, v_pinf);
+	BOOST_CHECK_EQUAL(v_ninf, v_ninf);
+	BOOST_CHECK_EQUAL(v_pinf, -v_ninf);
+	BOOST_CHECK_EQUAL(-v_pinf, v_ninf);
+
+	BOOST_CHECK(v_ninf < v_pinf);
+	BOOST_CHECK(v_ninf <= v_pinf);
+	BOOST_CHECK(v_ninf != v_pinf);
+	BOOST_CHECK(v_pinf >= v_ninf);
+	BOOST_CHECK(v_pinf > v_ninf);
+
+	BOOST_CHECK_EQUAL(v_pinf, v_pinf * 0.5);
+	BOOST_CHECK_EQUAL(v_pinf, v_pinf * 1.0);
+	BOOST_CHECK_EQUAL(v_pinf, v_pinf * 2.0);
+	BOOST_CHECK_EQUAL(v_pinf, v_pinf * v_pinf);
+	BOOST_CHECK_EQUAL(v_pinf, v_ninf * v_ninf);
+	BOOST_CHECK_EQUAL(v_ninf, v_ninf * 0.5);
+	BOOST_CHECK_EQUAL(v_ninf, v_ninf * 1.0);
+	BOOST_CHECK_EQUAL(v_ninf, v_ninf * 2.0);
+
+	BOOST_CHECK_EQUAL(v_ninf, v_pinf * -0.5);
+	BOOST_CHECK_EQUAL(v_ninf, v_pinf * -1.0);
+	BOOST_CHECK_EQUAL(v_ninf, v_pinf * -2.0);
+	BOOST_CHECK_EQUAL(v_pinf, v_ninf * -0.5);
+	BOOST_CHECK_EQUAL(v_pinf, v_ninf * -1.0);
+	BOOST_CHECK_EQUAL(v_pinf, v_ninf * -2.0);
+}
+
 BOOST_AUTO_TEST_CASE(basic)
 {
 	extended_double v0;
