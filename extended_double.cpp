a9ec01c461625c3b2d5774e62c59ef128a40c120
Florian G. Pflug <fgp@phlo.org>
2015-08-21 00:59:37 +0200

    Rewrote normalize_slowpath in the same spirit as normalize_after_multiply_slowpath

--- extended_double.cpp
+++ extended_double.cpp
@@ -74,47 +74,61 @@ void extended_double::normalize_after_multiply_slowpath() {
 }
 
 void extended_double::normalize_slowpath() {
-	/* Make fields of native double "m_fraction" available */
-	ieee754_double_t v;
-	v.as_double = m_fraction;
+    /* Make fields of native double "m_fraction" available */
+    ieee754_double_t f;
+    f.as_double = m_fraction;
+    const uint32_t f_e_raw = f.as_fields.exponent;
+    
+    if (ED_LIKELY(f_e_raw != IEEE754_DOUBLE_EXP_INF_RAW)) {
+        /* Fraction is neither +/- Infinity nor NaN */
 
-	/* Extract native exponent and determine if v is non-finite or sub-normal.
-     * For sub-normal values, e_type is 0, while for inifinities its 1.
-     * Note that zero is counted as being sub-normal here - as other sub-normal
-     * numbers, its native exponent is the smallest possible value.
-     */
-	const uint32_t e = uint32_t(v.as_fields.exponent);
-	const bool e_sub = (e == 0);
-	const bool e_inf = (e == IEEE754_DOUBLE_EXP_MASK);
+        /* Compute mask which is all-zero if m_fraction is zero, otherwise all-one */
+        const int64_t f_e_mask = -int32_t(f_e_raw > 0);
 
-	/* Update exponent field. For normals, the fraction's native exponent
-     * is added, rounded to a multiple of  FRACTION_RESCALING_THRESHOLD_LOG2.
-     * For sub-normals (incl. zeros) the exponent is set to the smallest value,
-     * while for infinities it is set to the largest.
-     */
-	const int32_t e_keep = (e + 1) % FRACTION_RESCALING_THRESHOLD_LOG2;
-	const int32_t e_val = int32_t(e) - int32_t(IEEE754_DOUBLE_EXP_EXCESS);
-	const int32_t e_delta = e_val - e_keep;
-	const int64_t e_adj = m_exponent_raw + e_delta;
-	m_exponent_raw = e_sub ? 0 : e_inf ? EXPONENT_EXCESS + EXPONENT_MAX : e_adj;
-
-	/* Update m_fraction's native exponent and mantissa. For non-finite
-     * and sub-normal values, the original exponent is kept - this preserves
-     * Zero, +/- Infinity and NaN. For normal values, the remainder of the
-     * exponent mod FRACTION_RESCALING_THRESHOLD_LOG2 is subtracted, since
-     * that part of the exponent was "moved" to the exponent field above.
-     * Sub-normal values are rounded down to zero by zeroing the mantissa,
-     * to guarantee that the result is normalized.  Note that doing this for
-     * all non-finite values would convert NaN to +/- Inf!
-     */
-	const uint32_t sp = uint32_t(v.as_fields.sign);
-	const uint32_t ep = (e_sub || e_inf) ? e : uint32_t(e_keep + int32_t(IEEE754_DOUBLE_EXP_EXCESS));
-	const uint64_t mp = e_sub ? 0 : v.as_fields.mantissa;
-	ieee754_double_t vp;
-	vp.as_uint64 = ((uint64_t(sp) << (IEEE754_DOUBLE_MAN_BITS + IEEE754_DOUBLE_EXP_BITS)) |
-	 			    (uint64_t(ep) << IEEE754_DOUBLE_MAN_BITS) |
-			        mp);
-	m_fraction = vp.as_double;
+        /* Compute native exponent mod FRACTION_RESCALING_THRESHOLD_LOG2.
+         * Result lies within [ 0, FRACTION_RESCALING_THRESHOLD_LOG2 ). This
+         * will become the new native exponent (after zero masking)
+         */
+        const int32_t f_e_n = (f_e_raw + 1) % FRACTION_RESCALING_THRESHOLD_LOG2;
+        
+        /* Compute updated explicit exponent. If the fraction is zero, the explicit
+         * exponent becomes zero as well (via masking). Otherwise, simply add
+         * the delta between the original (f_e) and the normalized (f_e_n)
+         * native exponent.
+         */
+        const int32_t f_e = int32_t(f_e_raw) - int32_t(IEEE754_DOUBLE_EXP_EXCESS);
+        const int32_t e_delta = f_e - f_e_n;
+        const int64_t e_p = (m_exponent_raw + e_delta) & f_e_mask;
+        
+        if (ED_UNLIKELY(e_p < 0)) {
+            /* Exponent underflowed. Set value to +/- Zero */
+            m_exponent_raw = 0;
+            f.as_fields.exponent = 0;
+            f.as_fields.mantissa = 0;
+            m_fraction = f.as_double;
+        }
+        else if (ED_LIKELY(e_p < EXPONENT_EXCESS + EXPONENT_MAX)) {
+            /* Exponent still valid. Update exponent and fraction.
+             * If the fraction was zero (or underflowed), masking ensures that
+             * it is set to zero.
+             */
+            m_exponent_raw = e_p;
+            f.as_fields.exponent = f_e_n + int32_t(IEEE754_DOUBLE_EXP_EXCESS);
+            f.as_uint64 &= f_e_mask | 0x8000000000000000;
+            m_fraction = f.as_double;
+        }
+        else {
+            /* Exponent overflowed. Set value to +/- Infinity */
+            m_exponent_raw = EXPONENT_EXCESS + EXPONENT_MAX;
+            f.as_fields.exponent = IEEE754_DOUBLE_EXP_INF_RAW;
+            f.as_fields.mantissa = 0;
+            m_fraction = f.as_double;
+        }
+    }
+    else {
+        /* Fraction has value +/- Infinity or NaN. Adjust exponent accordingly */
+        m_exponent_raw = EXPONENT_EXCESS + EXPONENT_MAX;
+    }
 }
 
 void
